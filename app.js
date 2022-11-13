6e1b35d5c63e3bcc1c4457254474f2dd3a04c448
Pablo Perez <lx00038@innovaccion.mx>
2022-11-12 19:51:35 -0600

    Segundo commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function moverPosicionRandom(elem){
+function moverPosicionRandom(elem) {
     elem.style.position = 'absolute';
     elem.style.top = Math.random() * (window.innerHeight - elem.offsetHeight) + 'px';
     elem.style.left = Math.random() * (window.innerWidth - elem.offsetWidth) + 'px';
@@ -6,14 +6,14 @@ function moverPosicionRandom(elem){
 
 let btnSi = document.getElementById("btn-si");
 let btnNo = document.getElementById("btn-no");
-let divModoCristobalColon = document.getElementsByClassName("modo_cristobal_colon")[0]
+let divConquistador = document.getElementsByClassName("modo_conquistador")[0]
 
 btnNo.addEventListener('mouseenter', function(e) {moverPosicionRandom(e.target)})
 
 btnSi.addEventListener('click', function(e){
     alert('Sabía que dirías que sí 😉❤️');
 
-    divModoCristobalColon.style.display = 'block';
+    divConquistador.style.display = 'block';
     const cancion = new Audio('img\\modo_hot.mp3');
     cancion.play();
 });
