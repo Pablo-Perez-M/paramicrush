20823da907d49b4c7aa4ae5f916a01744d58d5d7
Pablo Perez <lx00038@innovaccion.mx>
2022-11-12 20:07:55 -0600

    Tercer commit

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ function moverPosicionRandom(elem) {
 let btnSi = document.getElementById("btn-si");
 let btnNo = document.getElementById("btn-no");
 let divConquistador = document.getElementsByClassName("modo_conquistador")[0]
+let divVideo = document.getElementsByClassName("video");
 
 btnNo.addEventListener('mouseenter', function(e) {moverPosicionRandom(e.target)})
 
 btnSi.addEventListener('click', function(e) {
     alert('Sabía que dirías que sí 😉❤️');
+    divVideo.style.display = 'none';
 
     divConquistador.style.display = 'block';
     const cancion = new Audio('img\\modo_hot.mp3');
