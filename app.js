959873fbee2f516dcd59eaf102fd101a9f75cbf2
Pablo Perez <lx00038@innovaccion.mx>
2022-11-12 21:25:03 -0600

    cambio

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ btnNo.addEventListener('mouseenter', function(e) {moverPosicionRandom(e.target) 
 
 btnSi.addEventListener('click', function(e) {
     alert('Sabía que dirías que sí 😉❤️');
-
     divConquistador.style.display = 'block';
-    const cancion = new Audio('img\modo_hot.mp3');
+    const cancion = new Audio('img\\modo_hot.mp3');
     cancion.play();
 });
 
